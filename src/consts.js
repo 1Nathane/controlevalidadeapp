ed50328b7ae9f872fb7738ae7e74c8c07b564360
1Nathane <nathane.miranda@gmail.com>
2022-05-22 10:40:07 -0300

    ambiente dev

--- src/consts.js
+++ src/consts.js
@@ -1,4 +1,6 @@
 export default {
-    API_URL: 'https://controlevalidade-backend.herokuapp.com/api',
-    OAPI_URL: 'https://controlevalidade-backend.herokuapp.com/oapi',
+    // API_URL: 'https://controlevalidade-backend.herokuapp.com/api',
+    API_URL: 'https://controlevalidadeback-dev.herokuapp.com/api',
+    // OAPI_URL: 'https://controlevalidade-backend.herokuapp.com/oapi',
+    OAPI_URL: 'https://controlevalidadeback-dev.herokuapp.com/oapi',
 }
