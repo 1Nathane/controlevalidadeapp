498634cc7d35976105420771cdb9f69e020dc90a
1Nathane <nathane.miranda@gmail.com>
2022-05-25 00:00:47 -0300

    validation logi/signup

--- src/auth/authActions.js
+++ src/auth/authActions.js
@@ -5,12 +5,47 @@ import consts from '../consts'
 import { authConfig } from './Config'
 
 
+function validateSingup (values) {
+    var message = ''
+    var valid = true
+    if (!values['name'] || !values['email'] || !values['password'] || !values['confirm_password']) {
+        message = 'Todos os campos são obrigatórios!'
+        valid = false
+    }
+    if (!(values['password'] === values['confirm_password'])){
+        message = 'Senhas não conferem!'
+        valid = false
+    }
+    return {valid, message}
+}
+
+function validateLogin (values) {
+    var message = ''
+    var valid = true
+    if (!values['email'] || !values['password']) {
+        message = 'Todos os campos são obrigatórios!'
+        valid = false
+    }
+    return {valid, message}
+}
+
+
 export function login(values) {
-    return submit(authConfig.auth().signInWithEmailAndPassword(values['email'], values['password'])) 
+    const {valid, message} = validateLogin(values)
+    if(valid){
+        return submit(authConfig.auth().signInWithEmailAndPassword(values['email'], values['password'])) 
+    } else {
+        toastr.error('Erro', message)
+    }    
 }
 
 export function signup(values) {
+   const {valid, message} = validateSingup(values)
+   if(valid) {
     return submit(authConfig.auth().createUserWithEmailAndPassword(values['email'], values['password']))
+   } else {
+    toastr.error('Erro', message) 
+   }
 }
 
 function submit(response) {
